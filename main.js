5b395d2aa0e544fdfa88dd0f9051e6aef2749ee6
Kirill Butorin <butorink-00@yandex.ru>
2022-08-08 19:55:10 +0300

    Сделал рандомную генерацию числа

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 // 'use strict';
+const getRandomInt = function (max) {
+    return Math.floor(Math.random() * Math.floor(max));
+};
 const isNumber = function () {
     return !isNaN(parseFloat()) && isFinite();
 };
 
 function game() {
-    let x = 5;
+    let randomNumber = getRandomInt(100);
     function gameStart() {
         let number = prompt('Введите число от 1 до 100');
         let realNum = Number(number);
         if (number === null) {
             alert('Игра окончена');
-        } else if (realNum > x) {
+        } else if (realNum > randomNumber) {
             alert('Загаданное число меньше');
             gameStart();
-        } else if (realNum < x) {
+        } else if (realNum < randomNumber) {
             alert('Загаданное число больше');
             gameStart();
-        } else if (realNum === x) {
+        } else if (realNum === randomNumber) {
             alert('Поздравляю, Вы угадали!!!');
         } else if (realNum !== isNumber) {
             alert('Введите число');
